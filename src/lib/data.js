8c5c96c6550739200d97e4d29210cbcf37c521ae
CiroAntony <vasquezciro654@gmail.com>
2024-04-28 17:21:53 -0500

    upload project

---
+++ src/lib/data.js
@@ -0,0 +1,243 @@
+export const playlists = [
+    {
+        id: '1',
+        albumId: 1,
+        title: "Emo Shit",
+        color: "bg-gradient-to-b from-[#cc5400] to-zinc-900",
+        cover:
+            "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/rock-hits-mp3-cd-album-cover-template-design-2d9456fb8f1721d5a2f5fa7b78a6f886_screen.jpg?ts=1595860033",
+        artists: ["Skillet", "PXNDX", "Obus", "Paramore", "Thirty Seconds to Mars"],
+    },
+    {
+        id: '2',
+        albumId: 2,
+        title: "Reggae And Chill",
+        color: "bg-gradient-to-b from-[#da2735] to-zinc-900",
+        cover:
+            "https://i1.sndcdn.com/artworks-000024831849-cwgs97-t500x500.jpg",
+        artists: ["Bob Marley", "Damian Marley", "Stephen Marley", "Shaggy"],
+    },
+    {
+        id: '3',
+        albumId: 3,
+        title: "Old School Reggaeton",
+        color: "bg-gradient-to-b from-[#172bde] to-zinc-900",
+        cover:
+            "https://i.scdn.co/image/ab67616d0000b2739cc5f428e38ede9d45b4d552",
+        artists: ["Don Omar", "Wisin Y Yandel", "RKM & Ken-Y",],
+    },
+    {
+        id: '4',
+        albumId: 4,
+        title: "David Guetta Clasics",
+        color: "bg-gradient-to-b from-[#172bde] to-zinc-900",
+        cover:
+            "https://cdn.venngage.com/template/thumbnail/small/0171dbfd-9cba-446d-8920-990754182d11.webp",
+        artists: ["David Guetta"],
+    },
+]
+
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+
+
+export const songs = [
+    //playlist 1 : Teenagers Rock
+
+    {
+        "id": 1,
+        "albumId": 1,
+        "title": "Awake And Alive",
+        "image": `https://d1csarkz8obe9u.cloudfront.net/posterpreviews/rock-hits-mp3-cd-album-cover-template-design-2d9456fb8f1721d5a2f5fa7b78a6f886_screen.jpg?ts=1595860033`,
+        "artists": ["Skillet"],
+        "album": "Teengaers Rock",
+        "duration": "3:42"
+    },
+    {
+        "id": 2,
+        "albumId": 1,
+        "title": "The Kill",
+        "image": `https://d1csarkz8obe9u.cloudfront.net/posterpreviews/rock-hits-mp3-cd-album-cover-template-design-2d9456fb8f1721d5a2f5fa7b78a6f886_screen.jpg?ts=1595860033`,
+        "artists": ["Thirty Seconds To Mars"],
+        "album": "Teengaers Rock",
+        "duration": "5:30"
+    },
+    {
+        "id": 3,
+        "albumId": 1,
+        "title": "Ignorance",
+        "image": `https://d1csarkz8obe9u.cloudfront.net/posterpreviews/rock-hits-mp3-cd-album-cover-template-design-2d9456fb8f1721d5a2f5fa7b78a6f886_screen.jpg?ts=1595860033`,
+        "artists": ["Paramore"],
+        "album": "Teengaers Rock",
+        "duration": "3:40"
+    },
+    {
+        "id": 4,
+        "albumId": 1,
+        "title": "Narcicista Por Excelencia",
+        "image": `https://d1csarkz8obe9u.cloudfront.net/posterpreviews/rock-hits-mp3-cd-album-cover-template-design-2d9456fb8f1721d5a2f5fa7b78a6f886_screen.jpg?ts=1595860033`,
+        "artists": ["PXNDX"],
+        "album": "Teengaers Rock",
+        "duration": "3:51"
+    },
+    {
+        "id": 5,
+        "albumId": 1,
+        "title": "Te Visitará La Muerte",
+        "image": `https://d1csarkz8obe9u.cloudfront.net/posterpreviews/rock-hits-mp3-cd-album-cover-template-design-2d9456fb8f1721d5a2f5fa7b78a6f886_screen.jpg?ts=1595860033`,
+        "artists": ["Obus"],
+        "album": "Teengaers Rock",
+        "duration": "3:43"
+    },
+
+    //playlist 2: Reggae And Chill
+
+    {
+        "id": 1,
+        "albumId": 2,
+        "title": "Praise Jah In The Moonlight",
+        "image": `https://i1.sndcdn.com/artworks-000024831849-cwgs97-t500x500.jpg`,
+        "artists": ["YG Marley"],
+        "album": "Reggae And Chill",
+        "duration": "4:23"
+    },
+    {
+        "id": 2,
+        "albumId": 2,
+        "title": "Jamming",
+        "image": `https://i1.sndcdn.com/artworks-000024831849-cwgs97-t500x500.jpg`,
+        "artists": ["Bob Marley"],
+        "album": "Reggae And Chill",
+        "duration": "3:33"
+    },
+    {
+        "id": 3,
+        "albumId": 2,
+        "title": "Rasta Love",
+        "image": `https://i1.sndcdn.com/artworks-000024831849-cwgs97-t500x500.jpg`,
+        "artists": ["Protoje", "Ky-Mani Marley"],
+        "album": "Reggae And Chill",
+        "duration": "3:50"
+    },
+    {
+        "id": 4,
+        "albumId": 2,
+        "title": "Medication",
+        "image": `https://i1.sndcdn.com/artworks-000024831849-cwgs97-t500x500.jpg`,
+        "artists": ["Damian Marley", "Stephen Marley"],
+        "album": "Reggae And Chill",
+        "duration": "4:26"
+    },
+    {
+        "id": 5,
+        "albumId": 2,
+        "title": "Angel",
+        "image": `https://i1.sndcdn.com/artworks-000024831849-cwgs97-t500x500.jpg`,
+        "artists": ["Shaggy", "Rayvon"],
+        "album": "Reggae And Chill",
+        "duration": "3:55"
+    },
+
+        //playlist 3: Old School Reggaeton
+
+        {
+            "id": 1,
+            "albumId": 3,
+            "title": "Ojitos Chiquititos",
+            "image": `https://i.scdn.co/image/ab67616d0000b2739cc5f428e38ede9d45b4d552`,
+            "artists": ["Don Omar"],
+            "album": "Old School Reggaeton",
+            "duration": "3:49"
+        },
+        {
+            "id": 2,
+            "albumId": 3,
+            "title": "Nadie Como Tú",
+            "image": `https://i.scdn.co/image/ab67616d0000b2739cc5f428e38ede9d45b4d552`,
+            "artists": ["Don Omar", "Wisin Y Yandel"],
+            "album": "Old School Reggaeton",
+            "duration": "3:43"
+        },
+        {
+            "id": 3,
+            "albumId": 3,
+            "title": "Me Estas Tentando",
+            "image": `https://i.scdn.co/image/ab67616d0000b2739cc5f428e38ede9d45b4d552`,
+            "artists": ["Wisin Y Yandel"],
+            "album": "Old School Reggaeton",
+            "duration": "3:52"
+        },
+        {
+            "id": 4,
+            "albumId": 3,
+            "title": "Down",
+            "image": `https://i.scdn.co/image/ab67616d0000b2739cc5f428e38ede9d45b4d552`,
+            "artists": ["RKM & Ken-Y"],
+            "album": "Old School Reggaeton",
+            "duration": "3:45"
+        },
+        {
+            "id": 5,
+            "albumId": 3,
+            "title": "Me Matas",
+            "image": `https://i.scdn.co/image/ab67616d0000b2739cc5f428e38ede9d45b4d552`,
+            "artists": ["RKM & Ken-Y"],
+            "album": "Old School Reggaeton",
+            "duration": "3:16"
+        },
+
+        //Playlist 4: David Guetta clasics
+
+        {
+            "id": 1,
+            "albumId": 4,
+            "title": "Memories",
+            "image": `https://cdn.venngage.com/template/thumbnail/small/0171dbfd-9cba-446d-8920-990754182d11.webp`,
+            "artists": ["David Guetta", "Kid Cudi"],
+            "album": "David Guetta Clasics",
+            "duration": "3:29"
+        },
+        {
+            "id": 2,
+            "albumId": 4,
+            "title": "Sexy Chick",
+            "image": `https://cdn.venngage.com/template/thumbnail/small/0171dbfd-9cba-446d-8920-990754182d11.webp`,
+            "artists": ["David Guetta", "Akon"],
+            "album": "David Guetta Clasics",
+            "duration": "3:32"
+        },
+        {
+            "id": 3,
+            "albumId": 4,
+            "title": "Bad",
+            "image": `https://cdn.venngage.com/template/thumbnail/small/0171dbfd-9cba-446d-8920-990754182d11.webp`,
+            "artists": ["David Guetta", "Showtek"],
+            "album": "David Guetta Clasics",
+            "duration": "2:51"
+        },
+        {
+            "id": 4,
+            "albumId": 4,
+            "title": "Shot Me Down",
+            "image": `https://cdn.venngage.com/template/thumbnail/small/0171dbfd-9cba-446d-8920-990754182d11.webp`,
+            "artists": ["David Guetta", "Skylar Grey"],
+            "album": "David Guetta Clasics",
+            "duration": "3:13"
+        },
+        {
+            "id": 5,
+            "albumId": 4,
+            "title": "Where Them Girls At",
+            "image": `https://cdn.venngage.com/template/thumbnail/small/0171dbfd-9cba-446d-8920-990754182d11.webp`,
+            "artists": ["David Guetta", "Nicki Minaj", "Flo Rida"],
+            "album": "David Guetta Clasics",
+            "duration": "3:48"
+        },
+]
